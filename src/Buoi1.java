d5465976fbb761f1ac60a97461109d2cbbcf1b86
Tichhv <huynhtich@gmail.com>
2022-08-18 20:04:27 +0700

    update 2

--- src/Buoi1.java
+++ src/Buoi1.java
@@ -12,28 +12,28 @@ public class Buoi1 {
         System.out.print("Nhập cân nặng: ");
         double weight=  scanner.nextDouble();
 
-        double bmi =  Bmi(weight, height);
+        double bmi =  computeBmi(weight, height);
 
         String result;
 
         if (bmi <18.5)
         {
-            result = "Phân loại: Gầy - nguy cơ: Thấp";
+            result = "Phân loại: Gầy - Nguy cơ: Thấp";
         }
-        else if(bmi >= 18.5 && bmi <= 24.9)
+        else if ((bmi >= 18.5) && (bmi <= 24.9))
         {
             result = "Phân loại: Bình thường - Nguy cơ: Trung bình";
         }
-        else if(bmi >=25 && bmi<=29.9)
+        else if ((bmi >=25) && (bmi<=29.9))
         {
             result = "Phân loại: Hơi béo - Nguy cơ: Cao";
 
         }
-        else if(bmi >= 30 && bmi <= 34.9)
+        else if ((bmi >= 30) && (bmi <= 34.9))
         {
             result = "Phân loại: Béo phì cấp độ 1 - Nguy cơ: Cao";
         }
-        else if(bmi >= 35 && bmi <= 39.9)
+        else if ((bmi >= 35) && (bmi <= 39.9))
         {
             result = "Phân loại: Béo phì cấp độ 2 - Nguy cơ: Rất cao";
         }
@@ -44,8 +44,9 @@ public class Buoi1 {
 
         System.out.println("Bạn " + name + " Có chỉ số BMI là: " + bmi);
         System.out.println(result);
+
     }
-    public static double Bmi(double weight, double height)
+    public static double computeBmi(double weight, double height)
     {
         return weight/(height*height);
     }
